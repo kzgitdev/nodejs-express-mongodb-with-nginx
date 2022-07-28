bbd725f62b6bcf467e613ade35cbef66412b72ab
Kazuya.i <kzgitdev@outlook.com>
2022-07-28 16:39:08 +0900

    Update  server.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,26 @@
 'use strict';
-
+const Joi = require('joi');
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cron = require('node-cron'); // cron: 
+const date = require('date-utils'); // date timestamp
+process.env.TZ = 'Asia/Tokyo'; // set the timezone Japan Standard Time
+
+// ---  connect to mongodb --- //
+const {MongoClient} = require('mongodb');
+async function main() {
+}
+
+// cron unix time
+cron.schedule('* * * * * ', () => console.log(`*****: 毎分実行:  ${Math.floor(new Date().getTime() / 1000)}`));
+
 // App
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
 app.use(bodyParser.json())
 app.use(express.json());
-
 
 // Constants
 const PORT = 8080;
@@ -16,37 +30,82 @@ app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-// items
+// courses
 const courses = [
-  { id: 1, name: 'item1'},
-  { id: 2, name: 'item2'},
-  { id: 3, name: 'item3'},
+  { id: 1, name: 'course1'},
+  { id: 2, name: 'course2'},
+  { id: 3, name: 'course3'},
 ]
 
-// GET /api/items
-app.get('/api/items', (req, res) => {
-  res.send(items)
+// GET /api/courses
+app.get('/api/courses', (req, res) => {
+  res.send(courses)
 })
 
-// POST /api/items
-app.post('/api/items', (req, res) => {
-  const items = {
-    id: items.length + 1,
+// POST /api/courses
+app.post('/api/courses', (req, res) => {
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  res.send('400 error');
+
+  const course = {
+    id: courses.length + 1,
     name: req.body.name
   }
-  items.push(item)
-  res.send(item)
-  console.log(item)
+  courses.push(course);
+  res.send(course);
 })
 
-// GET /api/items/1
-app.get('/api/items/:id', (req, res) => {
-  const item = items.find(c => c.id === parseInt(req.items.id))
-  if (!item) res.status(404).send('The item with the given ID was not found')
-  res.send(item)
+// GET /api/courses/1
+app.get('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id))
+  if (!course) return res.status(404).send('The course with the given ID was not found')
+  res.send(course)
 })
+
 // PUT  /api/courses/1
+app.put('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id) );
+  if (!course) return res.status(400).send('The course with the given ID was not found.');
+  
+  const { error } = validateCourse(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+  course.name = req.body.name;
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+  return schema.validate(course);
+}
+
 // DELETE /api/courses/1
+app.delete('/api/courses/:id', (req, res) => {
+  const course = courses.find(c => c.id === parseInt(req.params.id) );
+  if (!course) return res.status(400).send('The course with the given ID was not found.');
+
+  // Delete
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  res.send(course);
+});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+// connect mongodb server
+const URL = 'mongodb://username:password@mongo/appdb?authSource=admin'
+const client = new MongoClient(URL)
+
+
+try {
+  // await
+  client.connect();
+  console.log('Succesfully connected to mongo.');
+} catch (e) {
+  // error
+  console.log(e);
+}
